3fbfe9b56e4bc06b172c6a8f714b7ebc65fb990f
Alex Martens <alex@thinglab.org>
2022-07-26 12:30:32 -0700

    Add configurable polling rates

--- module.nix
+++ module.nix
@@ -34,12 +34,35 @@ in
     elf = lib.mkOption {
       type = lib.types.str;
       example = "/path/to/binary";
+      description = ''
+        Path to the ELF file loaded on the target.
+
+        This is used to locate the RTT memory section.
+      '';
     };
 
     group = lib.mkOption {
       type = lib.types.str;
-      description = "Group with permissions to use the debug probe";
+      description = "Group with permissions to use the debug probe.";
       default = "rttdprobe";
+    };
+
+    connectUnderReset = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Connect to the target under reset.";
+    };
+
+    maxPollRateMillis = lib.mkOption {
+      type = lib.types.ints.u64;
+      description = "Maximum polling rate in milliseconds.";
+      default = 3000;
+    };
+
+    minPollRateMillis = lib.mkOption {
+      type = lib.types.ints.u64;
+      description = "Minimum polling rate in milliseconds.";
+      default = 10;
     };
   };
 
@@ -56,73 +79,78 @@ in
         MODE="0660"
     '';
 
-    systemd.services.rtt-daemon = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "dev-rttdprobe.device" ];
-      requires = [ "dev-rttdprobe.device" ];
-      description = "RTT daemon";
-      unitConfig.ReloadPropagatedFrom = "dev-rttdprobe.device";
-      serviceConfig = {
-        Type = "idle";
-        KillSignal = "SIGINT";
-        ExecStart = ''
-          ${pkgs.rtt-daemon}/bin/rtt-daemon \
-            ${cfg.chip} \
-            ${cfg.probeVid}:${cfg.probePid}:${cfg.probeSerial} \
-            --elf ${cfg.elf}
-        '';
-        Restart = "on-failure";
-        RestartSec = 10;
+    systemd.services.rtt-daemon =
+      let
+        configFile = pkgs.writeText "rtt-daemon-config.json" (builtins.toJSON {
+          inherit (cfg) chip elf;
+          probe = "${cfg.probeVid}:${cfg.probePid}:${cfg.probeSerial}";
+          connect_under_reset = cfg.connectUnderReset;
+          min_poll_rate_millis = cfg.minPollRateMillis;
+          max_poll_rate_millis = cfg.maxPollRateMillis;
+        });
+      in
+      {
+        wantedBy = [ "multi-user.target" ];
+        after = [ "dev-rttdprobe.device" ];
+        requires = [ "dev-rttdprobe.device" ];
+        description = "RTT daemon";
+        unitConfig.ReloadPropagatedFrom = "dev-rttdprobe.device";
+        serviceConfig = {
+          Type = "idle";
+          KillSignal = "SIGINT";
+          ExecStart = "${pkgs.rtt-daemon}/bin/rtt-daemon ${configFile}";
+          Restart = "on-failure";
+          RestartSec = 10;
 
-        # hardening
-        SupplementaryGroups = [ cfg.group ];
-        DynamicUser = true;
-        DevicePolicy = "closed";
-        CapabilityBoundingSet = "";
-        RestrictAddressFamilies = [
-          "AF_INET"
-          "AF_INET6"
-          "AF_NETLINK"
-          "AF_UNIX"
-        ];
-        DeviceAllow = [
-          "char-usb_device rwm"
-        ];
-        NoNewPrivileges = true;
-        PrivateDevices = true;
-        PrivateMounts = true;
-        PrivateTmp = true;
-        PrivateUsers = true;
-        ProtectClock = true;
-        ProtectControlGroups = true;
-        ProtectHome = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        ProtectSystem = "strict";
-        BindPaths = [
-          "/dev/bus/usb"
-          "-${cfg.elf}"
-        ];
-        MemoryDenyWriteExecute = true;
-        LockPersonality = true;
-        RemoveIPC = true;
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        SystemCallArchitectures = "native";
-        SystemCallFilter = [
-          "~@debug"
-          "~@mount"
-          "~@privileged"
-          "~@resources"
-          "~@cpu-emulation"
-          "~@obsolete"
-        ];
-        ProtectProc = "invisible";
-        ProtectHostname = true;
-        ProcSubset = "pid";
+          # hardening
+          SupplementaryGroups = [ cfg.group ];
+          DynamicUser = true;
+          DevicePolicy = "closed";
+          CapabilityBoundingSet = "";
+          RestrictAddressFamilies = [
+            "AF_INET"
+            "AF_INET6"
+            "AF_NETLINK"
+            "AF_UNIX"
+          ];
+          DeviceAllow = [
+            "char-usb_device rwm"
+          ];
+          NoNewPrivileges = true;
+          PrivateDevices = true;
+          PrivateMounts = true;
+          PrivateTmp = true;
+          PrivateUsers = true;
+          ProtectClock = true;
+          ProtectControlGroups = true;
+          ProtectHome = true;
+          ProtectKernelLogs = true;
+          ProtectKernelModules = true;
+          ProtectKernelTunables = true;
+          ProtectSystem = "strict";
+          BindPaths = [
+            "/dev/bus/usb"
+            "-${cfg.elf}"
+          ];
+          MemoryDenyWriteExecute = true;
+          LockPersonality = true;
+          RemoveIPC = true;
+          RestrictNamespaces = true;
+          RestrictRealtime = true;
+          RestrictSUIDSGID = true;
+          SystemCallArchitectures = "native";
+          SystemCallFilter = [
+            "~@debug"
+            "~@mount"
+            "~@privileged"
+            "~@resources"
+            "~@cpu-emulation"
+            "~@obsolete"
+          ];
+          ProtectProc = "invisible";
+          ProtectHostname = true;
+          ProcSubset = "pid";
+        };
       };
-    };
   };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use anyhow::Context;
-use clap::Parser;
 use probe_rs::{config::MemoryRegion, Core, DebugProbeSelector, Probe, Session, Target};
 use probe_rs_rtt::{Rtt, ScanRegion, UpChannel};
+use serde::Deserialize;
 use std::{
+    ffi::OsString,
+    fs::File,
+    io::BufReader,
     path::{Path, PathBuf},
     thread::sleep,
     time::Duration,
 };
-
-const DEFAULT_SLEEP: Duration = Duration::from_millis(10);
-const MAX_SLEEP: Duration = Duration::from_secs(3);
 
 fn rtt_region<P>(elf_path: P) -> anyhow::Result<ScanRegion>
 where
@@ -17,8 +17,8 @@ where
 {
     use object::{Object, ObjectSymbol, Symbol};
 
-    let bin_data: Vec<u8> = std::fs::read(elf_path).context("failed to read ELF file")?;
-    let obj_file = object::File::parse(&*bin_data).context("failed to parse ELF file")?;
+    let bin_data: Vec<u8> = std::fs::read(elf_path).context("Failed to read ELF file")?;
+    let obj_file = object::File::parse(&*bin_data).context("Failed to parse ELF file")?;
 
     let symbol: Symbol = obj_file
         .symbols()
@@ -31,55 +31,84 @@ where
     Ok(ScanRegion::Exact(addr))
 }
 
-#[derive(Parser, Debug)]
-#[clap(about, version, author)]
-struct Args {
+#[derive(Deserialize)]
+struct Config {
     /// Target chip.
-    #[clap(value_parser)]
     chip: String,
     /// Probe to use, 'VID:PID' or 'VID:PID:Serial'.
-    #[clap(arg_enum, value_parser)]
-    probe: DebugProbeSelector,
+    probe: String,
     /// Path to the ELF file to speed up locating the RTT control block.
-    #[clap(long, value_parser)]
     elf: Option<PathBuf>,
-    /// Connect to the target under reset
-    #[clap(long, action)]
+    /// Connect to the target under reset.
     connect_under_reset: bool,
+    /// Minimum polling rate in milliseconds.
+    min_poll_rate_millis: u64,
+    /// Maximum polling rate in milliseconds.
+    max_poll_rate_millis: u64,
 }
 
 fn main() -> anyhow::Result<()> {
     ctrlc::set_handler(|| std::process::exit(0)).context("failed to set SIGINT handler")?;
 
-    systemd_journal_logger::init().context("failed to initialize logging")?;
+    systemd_journal_logger::init().context("Failed to initialize logging")?;
     log::set_max_level(log::LevelFilter::Info);
 
-    let args: Args = Args::parse();
+    let config_file_path: OsString = match std::env::args_os().nth(1) {
+        Some(x) => x,
+        None => {
+            eprintln!(
+                "usage: {} [config-file]",
+                std::env::args_os()
+                    .next()
+                    .unwrap_or_else(|| OsString::from("???"))
+                    .to_string_lossy()
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let config: Config = serde_json::from_reader(BufReader::new(
+        File::open(config_file_path).context("Failed to open configuration file")?,
+    ))
+    .context("Failed to load configuration from file")?;
+
+    if config.min_poll_rate_millis > config.max_poll_rate_millis {
+        anyhow::bail!(
+            "Minimum pollling rate of {:?} exceeds maximum polling rate of {:?}",
+            Duration::from_millis(config.min_poll_rate_millis),
+            Duration::from_millis(config.max_poll_rate_millis)
+        );
+    }
 
     let target: Target =
-        probe_rs::config::get_target_by_name(args.chip).context("chip not found")?;
+        probe_rs::config::get_target_by_name(config.chip).context("Chip not found")?;
+
+    let selector: DebugProbeSelector = config
+        .probe
+        .try_into()
+        .context("Probe selector is invalid")?;
 
     log::info!("Opening probe");
-    let probe: Probe = Probe::open(args.probe).context("failed to open probe")?;
+    let probe: Probe = Probe::open(selector).context("Failed to open probe")?;
 
     log::info!("Attaching to target");
     let permissions = probe_rs::Permissions::new();
-    let mut session: Session = if args.connect_under_reset {
+    let mut session: Session = if config.connect_under_reset {
         probe.attach_under_reset(target, permissions)
     } else {
         probe.attach(target, permissions)
     }
-    .context("failed to attach to the target")?;
+    .context("Failed to attach to the target")?;
 
     let memory_map: Vec<MemoryRegion> = session.target().memory_map.clone();
 
-    let mut core: Core = session.core(0).context("failed to attach to core 0")?;
+    let mut core: Core = session.core(0).context("Failed to attach to core 0")?;
 
-    let scan_region: Option<ScanRegion> = if let Some(elf) = args.elf {
+    let scan_region: Option<ScanRegion> = if let Some(elf) = config.elf {
         match rtt_region(elf) {
             Ok(region) => Some(region),
             Err(e) => {
-                log::error!("failed to get RTT region from ELF: {e}");
+                log::error!("Failed to get RTT region from ELF: {e}");
                 None
             }
         }
@@ -94,32 +123,32 @@ fn main() -> anyhow::Result<()> {
         log::info!("Attaching to RTT region");
         Rtt::attach(&mut core, &memory_map)
     }
-    .context("failed to attach to RTT")?;
+    .context("Failed to attach to RTT")?;
 
     let upch: UpChannel = rtt
         .up_channels()
         .take(0)
-        .context("failed to attach to RTT up channel 0")?;
+        .context("Failed to attach to RTT up channel 0")?;
 
-    let mut sleep_time: Duration = DEFAULT_SLEEP;
+    let mut sleep_time_millis: u64 = config.min_poll_rate_millis;
     log::info!("Entering main loop");
     loop {
         let buf: Vec<u8> = {
             let mut buf: Vec<u8> = vec![0; 64 * 1024];
             let n_bytes: usize = upch
                 .read(&mut core, &mut buf)
-                .context("failed to read RTT channel")?;
+                .context("Failed to read RTT channel")?;
             buf.truncate(n_bytes);
             buf
         };
 
         if buf.is_empty() {
-            sleep(sleep_time);
-            if sleep_time < MAX_SLEEP {
-                sleep_time *= 2;
+            sleep(Duration::from_millis(sleep_time_millis));
+            if sleep_time_millis < config.max_poll_rate_millis {
+                sleep_time_millis = sleep_time_millis.saturating_mul(2);
             }
         } else {
-            sleep_time = DEFAULT_SLEEP;
+            sleep_time_millis = config.min_poll_rate_millis;
 
             let data: String = match std::str::from_utf8(&buf) {
                 Ok(s) => s.to_string(),
