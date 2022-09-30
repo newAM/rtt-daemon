369086cf78338f0fda24f495ed671d45cb58e3d7
Alex Martens <alex@thinglab.org>
2022-09-30 07:18:33 -0700

    nixpkgs-fmt -> alejandra

--- flake.nix
+++ flake.nix
@@ -10,59 +10,65 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+  }:
     nixpkgs.lib.recursiveUpdate
-      (flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ]
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
-            craneLib = crane.lib.${system};
+    (flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"]
+      (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+          cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
+          craneLib = crane.lib.${system};
 
-            commonArgs = {
-              src = ./.;
-              nativeBuildInputs = with pkgs; [
-                pkg-config
-              ];
-              buildInputs = with pkgs; [
-                libusb1
-                udev
-              ];
-            };
+          commonArgs = {
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+            buildInputs = with pkgs; [
+              libusb1
+              udev
+            ];
+          };
 
-            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-          in
-          rec {
-            packages.default = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in rec {
+          packages.default = craneLib.buildPackage (commonArgs
+            // {
               inherit cargoArtifacts;
             });
 
-            checks = {
-              pkg = packages.default;
+          checks = {
+            pkg = packages.default;
 
-              clippy = craneLib.cargoClippy (commonArgs // {
+            clippy = craneLib.cargoClippy (commonArgs
+              // {
                 inherit cargoArtifacts;
                 cargoClippyExtraArgs = "-- --deny warnings";
               });
 
-              rustfmt = craneLib.cargoFmt { src = ./.; };
+            rustfmt = craneLib.cargoFmt {src = ./.;};
 
-              nixpkgs-fmt = pkgs.runCommand "nixpkgs-fmt" { } ''
-                ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
-                touch $out
-              '';
+            alejandra = pkgs.runCommand "alejandra" {} ''
+              ${pkgs.alejandra}/bin/alejandra --check ${./.}
+              touch $out
+            '';
 
-              statix = pkgs.runCommand "statix" { } ''
-                ${pkgs.statix}/bin/statix check ${./.}
-                touch $out
-              '';
-            };
-          }
-        ))
-      {
-        overlays.default = final: prev: {
-          rtt-daemon = self.packages.${prev.system}.default;
-        };
-        nixosModules.default = import ./module.nix;
+            statix = pkgs.runCommand "statix" {} ''
+              ${pkgs.statix}/bin/statix check ${./.}
+              touch $out
+            '';
+          };
+        }
+      ))
+    {
+      overlays.default = final: prev: {
+        rtt-daemon = self.packages.${prev.system}.default;
       };
+      nixosModules.default = import ./module.nix;
+    };
 }
--- module.nix
+++ module.nix
@@ -1,10 +1,11 @@
-{ config, lib, pkgs, ... }:
-
-
-let
-  cfg = config.services.rtt-daemon;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.rtt-daemon;
+in {
   options.services.rtt-daemon = with lib; {
     enable = mkEnableOption "rtt-daemon";
 
@@ -69,7 +70,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    users.groups."${cfg.group}" = { };
+    users.groups."${cfg.group}" = {};
 
     services.udev.extraRules = ''
       SUBSYSTEMS=="usb", \
@@ -81,78 +82,76 @@ in
         MODE="0660"
     '';
 
-    systemd.services.rtt-daemon =
-      let
-        configFile = pkgs.writeText "rtt-daemon-config.json" (builtins.toJSON {
-          inherit (cfg) chip elf;
-          probe = "${cfg.probeVid}:${cfg.probePid}:${cfg.probeSerial}";
-          connect_under_reset = cfg.connectUnderReset;
-          min_poll_rate_millis = cfg.minPollRateMillis;
-          max_poll_rate_millis = cfg.maxPollRateMillis;
-        });
-      in
-      {
-        wantedBy = [ "multi-user.target" ];
-        after = [ "dev-rttdprobe.device" ];
-        requires = [ "dev-rttdprobe.device" ];
-        description = "RTT daemon";
-        unitConfig.ReloadPropagatedFrom = "dev-rttdprobe.device";
-        serviceConfig = {
-          Type = "idle";
-          KillSignal = "SIGINT";
-          ExecStart = "${pkgs.rtt-daemon}/bin/rtt-daemon ${configFile}";
-          Restart = "on-failure";
-          RestartSec = 10;
+    systemd.services.rtt-daemon = let
+      configFile = pkgs.writeText "rtt-daemon-config.json" (builtins.toJSON {
+        inherit (cfg) chip elf;
+        probe = "${cfg.probeVid}:${cfg.probePid}:${cfg.probeSerial}";
+        connect_under_reset = cfg.connectUnderReset;
+        min_poll_rate_millis = cfg.minPollRateMillis;
+        max_poll_rate_millis = cfg.maxPollRateMillis;
+      });
+    in {
+      wantedBy = ["multi-user.target"];
+      after = ["dev-rttdprobe.device"];
+      requires = ["dev-rttdprobe.device"];
+      description = "RTT daemon";
+      unitConfig.ReloadPropagatedFrom = "dev-rttdprobe.device";
+      serviceConfig = {
+        Type = "idle";
+        KillSignal = "SIGINT";
+        ExecStart = "${pkgs.rtt-daemon}/bin/rtt-daemon ${configFile}";
+        Restart = "on-failure";
+        RestartSec = 10;
 
-          # hardening
-          SupplementaryGroups = [ cfg.group ];
-          DynamicUser = true;
-          DevicePolicy = "closed";
-          CapabilityBoundingSet = "";
-          RestrictAddressFamilies = [
-            "AF_INET"
-            "AF_INET6"
-            "AF_NETLINK"
-            "AF_UNIX"
-          ];
-          DeviceAllow = [
-            "char-usb_device rwm"
-          ];
-          NoNewPrivileges = true;
-          PrivateDevices = true;
-          PrivateMounts = true;
-          PrivateTmp = true;
-          PrivateUsers = true;
-          ProtectClock = true;
-          ProtectControlGroups = true;
-          ProtectHome = true;
-          ProtectKernelLogs = true;
-          ProtectKernelModules = true;
-          ProtectKernelTunables = true;
-          ProtectSystem = "strict";
-          BindPaths = [
-            "/dev/bus/usb"
-            "-${cfg.elf}"
-          ];
-          MemoryDenyWriteExecute = true;
-          LockPersonality = true;
-          RemoveIPC = true;
-          RestrictNamespaces = true;
-          RestrictRealtime = true;
-          RestrictSUIDSGID = true;
-          SystemCallArchitectures = "native";
-          SystemCallFilter = [
-            "~@debug"
-            "~@mount"
-            "~@privileged"
-            "~@resources"
-            "~@cpu-emulation"
-            "~@obsolete"
-          ];
-          ProtectProc = "invisible";
-          ProtectHostname = true;
-          ProcSubset = "pid";
-        };
+        # hardening
+        SupplementaryGroups = [cfg.group];
+        DynamicUser = true;
+        DevicePolicy = "closed";
+        CapabilityBoundingSet = "";
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+          "AF_NETLINK"
+          "AF_UNIX"
+        ];
+        DeviceAllow = [
+          "char-usb_device rwm"
+        ];
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateMounts = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        BindPaths = [
+          "/dev/bus/usb"
+          "-${cfg.elf}"
+        ];
+        MemoryDenyWriteExecute = true;
+        LockPersonality = true;
+        RemoveIPC = true;
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "~@debug"
+          "~@mount"
+          "~@privileged"
+          "~@resources"
+          "~@cpu-emulation"
+          "~@obsolete"
+        ];
+        ProtectProc = "invisible";
+        ProtectHostname = true;
+        ProcSubset = "pid";
       };
+    };
   };
 }
