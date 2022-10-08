510fe4630154f54d475f07fcf681b864870ee136
Alex Martens <alex@thinglab.org>
2022-10-08 14:57:50 -0700

    flake: filter sources

--- flake.nix
+++ flake.nix
@@ -17,49 +17,46 @@
     flake-utils,
   }:
     nixpkgs.lib.recursiveUpdate
-    (flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"]
-      (
+    (flake-utils.lib.eachSystem [
+        "aarch64-linux"
+        "x86_64-linux"
+      ] (
         system: let
           pkgs = nixpkgs.legacyPackages.${system};
           cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
           craneLib = crane.lib.${system};
 
-          commonArgs = {
-            src = ./.;
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-            ];
-            buildInputs = with pkgs; [
-              libusb1
-              udev
-            ];
+          src = craneLib.cleanCargoSource ./.;
+          nativeBuildInputs = with pkgs; [pkg-config];
+          buildInputs = with pkgs; [libusb1 udev];
+
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src nativeBuildInputs buildInputs;
           };
 
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        in rec {
-          packages.default = craneLib.buildPackage (commonArgs
-            // {
-              inherit cargoArtifacts;
-            });
+          nixSrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".nix"];
+        in {
+          packages.default = craneLib.buildPackage {
+            inherit src nativeBuildInputs buildInputs cargoArtifacts;
+          };
 
           checks = {
-            pkg = packages.default;
+            pkg = self.packages.${system}.default;
 
-            clippy = craneLib.cargoClippy (commonArgs
-              // {
-                inherit cargoArtifacts;
-                cargoClippyExtraArgs = "-- --deny warnings";
-              });
+            clippy = craneLib.cargoClippy {
+              inherit src nativeBuildInputs buildInputs cargoArtifacts;
+              cargoClippyExtraArgs = "-- --deny warnings";
+            };
 
-            rustfmt = craneLib.cargoFmt {src = ./.;};
+            rustfmt = craneLib.cargoFmt {inherit src;};
 
             alejandra = pkgs.runCommand "alejandra" {} ''
-              ${pkgs.alejandra}/bin/alejandra --check ${./.}
+              ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
               touch $out
             '';
 
             statix = pkgs.runCommand "statix" {} ''
-              ${pkgs.statix}/bin/statix check ${./.}
+              ${pkgs.statix}/bin/statix check ${nixSrc}
               touch $out
             '';
           };
