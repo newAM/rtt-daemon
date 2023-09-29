46559168f98c0cdde2e3a52b83a9d34158756532
Alex Martens <alex@thinglab.org>
2023-09-29 16:44:41 -0700

    rm statix check

--- flake.nix
+++ flake.nix
@@ -54,11 +54,6 @@
               ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
               touch $out
             '';
-
-            statix = pkgs.runCommand "statix" {} ''
-              ${pkgs.statix}/bin/statix check ${nixSrc}
-              touch $out
-            '';
           };
         }
       ))
